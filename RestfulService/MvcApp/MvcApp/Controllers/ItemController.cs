5847cdddbafa431a450becccccea3b9e32f9501e
Brent van der Merwe <brent@smd.co.za>
2014-11-25 10:27:02 +0200

    created webapi project

---
+++ RestfulService/MvcApp/MvcApp/Controllers/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web.Http;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+	public class ItemController : ApiController
+	{
+		[HttpGet]
+		public IEnumerable Get()
+		{
+			var items = new List<Item>()
+			{
+				new Item() {Name = "Rahul"},
+				new Item() {Name = "Chinmoy"},
+				new Item() {Name = "Charu"}
+			};
+
+			return items;
+		}
+	}
+}
