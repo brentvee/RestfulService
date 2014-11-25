377162d43bf0bc6dfe3984311587a02d88bf91bf
Brent van der Merwe <brent@smd.co.za>
2014-11-25 09:08:26 +0200

    added project framework

---
+++ RestfulService/RestfulService/RestfulService/Contracts/Services/IRestulService.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+using RestfulService.Contracts.Data;
+
+namespace RestfulService.Contracts.Services
+{
+	
+	[ServiceContract]
+	public interface IRestulService
+	{
+
+		[OperationContract]
+		string GetData(int value);
+
+		[OperationContract]
+		CompositeType GetDataUsingDataContract(CompositeType composite);
+
+	}
+}
